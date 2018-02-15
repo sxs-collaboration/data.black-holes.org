a984cd95ba25864949e529e74a426b3a6d9e05fe
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-02-15 12:40:10 -0800

    Email committers of SimulationAnnex data that hasn't been copied

--- cron/scripts/git/git_annex_get_all_data.sh
+++ cron/scripts/git/git_annex_get_all_data.sh
@@ -10,4 +10,4 @@ git pull --rebase --quiet origin master
 git annex merge > /dev/null
 
 # Look for broken symlinks, and run `git annex get` on each
-find . -not -path './hooks' -not -path './Support' -xtype l -exec git annex get {} \;
+find . -not -path './hooks' -not -path './Support' -not -path './*Links' -xtype l -print0 | xargs -0 /web/servers/cron/scripts/git/git_annex_get_all_data.py
