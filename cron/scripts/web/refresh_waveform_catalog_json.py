34b2ab895e62c142b08941b3e60fd34d5451eee2
Michael Boyle <michael.oliver.boyle@gmail.com>
2018-02-15 09:58:58 -0800

    Correct paths to update file listings for catalog

--- cron/scripts/web/refresh_waveform_catalog_json.py
+++ cron/scripts/web/refresh_waveform_catalog_json.py
@@ -5,5 +5,6 @@ from sxs.metadata import create_web_files
 create_web_files(catalog_root_directory='/sxs-annex/SimulationAnnex.git',
                  relative_directory_path='../SimulationAnnex.git',
                  public_json_directory='/web/servers/data/document_root/waveforms',
+                 public_file_listings_directory='/web/servers/data/document_root/waveforms/data',
                  public_links_directory='/web/servers/data/document_root/waveforms/files',
                  private_json_directory='/web/servers/www/wiki/document_root/data/media/website/data/waveforms')
